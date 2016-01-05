a487093263b14eb17f00ab3209e92b16022a55a5
apple <apple@appletekimacbook-pro-2.local>
2016-01-05 14:50:32 +0800

    Initial Commit

---
+++ SimpleNetwork/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SimpleNetwork
+//
+//  Created by apple on 16/1/5.
+//  Copyright © 2016年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SimpleNetwork/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SimpleNetwork
+//
+//  Created by apple on 16/1/5.
+//  Copyright © 2016年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
